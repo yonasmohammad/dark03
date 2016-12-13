df77fa53a4ecef7741fe29bfb1f7eea8a3872f41
yonas.mohammad <toro.yonas.mohammad@gmail.com>
2016-12-13 11:10:24 +0800

    all

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+'use strict';
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+  return gulp.src('src/scss/index.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('app/assets/css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch(['src/scss/*.scss', 'src/scss/**/*.scss'], ['sass']);
+});
+
+
+gulp.task('default', ['sass'])
+
+
